b90462b8a737860500f9eeeacd368b94be695dc9
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-10-05 15:51:39 +0200

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,30 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
+  /*
+    ❗ Read the README.md first! Below is a strategy you can follow to tackle the challenge:
+
+    * Fetch learners using Axios or fetch
+        * Fetch mentors using Axios or fetch
+            * Create a variable to hold the combined data
+            * Combine the learners and the mentors to form an array like so:
+                [etc
+                  {
+                    id: 22,
+                    email:"mickey.mouse@example.com",
+                    fullName: "Mickey Mouse",
+                    mentors: ['James Gosling', 'Mary Shaw'] // ❗ actual names instead of IDs!
+                  },
+                etc]
+            * Loop over the array holding the formatted data
+                * For each object, make a `div.card` with all its children elements
+                * For each mentor name within each object, populate the `ul` inside the card
+                * Add a click handler to the `div.card` that activates it and deactivates it
+
+    Have fun!
+  */
+
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
